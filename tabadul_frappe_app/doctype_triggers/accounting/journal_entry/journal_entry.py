ded4e0d8b21762682c9a7d0b3477becfb3edcba5
Your Name <you@example.com>
2023-07-18 19:18:20 +0400

    Update JL

--- tabadul_frappe_app/doctype_triggers/accounting/journal_entry/journal_entry.py
+++ tabadul_frappe_app/doctype_triggers/accounting/journal_entry/journal_entry.py
@@ -19,31 +19,32 @@ def before_validate(doc, method=None):
     pass
 @frappe.whitelist()
 def validate(doc, method=None):
-    for d in doc.accounts:
-        if d.user_remark:
-            html_string = str(d.user_remark)
-            soup = BeautifulSoup(html_string, 'html.parser')
-            plain_text = soup.get_text()
-            #frappe.throw(plain_text)
-            all_descriptions = frappe.db.sql(
-            """
-            SELECT name
-            from `tabDescription Dimensions` 
-            where `tabDescription Dimensions`.name = '{escaped_string}' 
-            """.format(escaped_string=plain_text), as_dict=1)
-            #all_descriptions = frappe.db.get_list('Description Dimensions', filters={'name': escaped_string})
-            if all_descriptions :
-                for m in all_descriptions :
-                    descriptions = m.name
-                    d.description_dimensions = descriptions
-            if not all_descriptions:
-                new_doc = frappe.get_doc(dict(
-                doctype='Description Dimensions',
-                name=plain_text,
-                description_dimensions=plain_text,
-                ))
-                new_doc.insert(ignore_permissions=True)
-                d.description_dimensions = new_doc.name
+    pass
+    # for d in doc.accounts:
+    #     if d.user_remark:
+    #         html_string = str(d.user_remark)
+    #         soup = BeautifulSoup(html_string, 'html.parser')
+    #         plain_text = soup.get_text()
+    #         #frappe.throw(plain_text)
+    #         all_descriptions = frappe.db.sql(
+    #         """
+    #         SELECT name
+    #         from `tabDescription Dimensions` 
+    #         where `tabDescription Dimensions`.name = '{escaped_string}' 
+    #         """.format(escaped_string=plain_text), as_dict=1)
+    #         #all_descriptions = frappe.db.get_list('Description Dimensions', filters={'name': escaped_string})
+    #         if all_descriptions :
+    #             for m in all_descriptions :
+    #                 descriptions = m.name
+    #                 d.description_dimensions = descriptions
+    #         if not all_descriptions:
+    #             new_doc = frappe.get_doc(dict(
+    #             doctype='Description Dimensions',
+    #             name=plain_text,
+    #             description_dimensions=plain_text,
+    #             ))
+    #             new_doc.insert(ignore_permissions=True)
+    #             d.description_dimensions = new_doc.name
 @frappe.whitelist()
 def on_submit(doc, method=None):
     pass
